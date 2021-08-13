e3e4f136e89ec56c2cc775d5a013ac5081254e6c
GabrieleLaMAlfa <g.lamalfa@jbs.cam.ac.uk>
2021-08-13 12:46:38 +0100

    Simple Linear Regression

--- fit_linear.py
+++ fit_linear.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 
@@ -15,13 +16,10 @@ class LinearRegressionFit:
         self.msg = None
         self.df = []
         self.file = None
-        self.train_share = None
-        self.test_share = None
-        self.train_set_x = None
-        self.train_set_y = None
-        self.test_set = None
-        self.test_set_x = None
-        self.test_set_y = None
+        self.x_train = None
+        self.x_test = None
+        self.y_train = None
+        self.y_test = None
         self.prediction = None
 
     def process_inputs(self, upload_folder, upload_files, setup_sheet_name):
@@ -39,41 +37,37 @@ class LinearRegressionFit:
         return None
 
     def train_test_sets(self, index_name, train_percentage):
-        seq_train = pd.DataFrame()
-        seq_test = pd.DataFrame()
-        self.train_share = int(len(self.df[0]) * train_percentage + 1)
-        self.test_share = int(len(self.df[0]) * (1-train_percentage))
-        seq_train['sequence_train'] = pd.Series(range(self.train_share))
-        seq_test['sequence_test'] = pd.Series(range(self.test_share))
-
-        self.train_set_x = seq_train['sequence_train'].values.reshape(-1, 1)
-        self.train_set_y = self.df[0][index_name][:self.train_share].values.reshape(-1, 1)
-        self.test_set_x = seq_test['sequence_test'].values.reshape(-1, 1)
-        self.test_set_y = self.df[0][index_name][self.train_share:].values.reshape(-1, 1)
+        x = self.df[0][index_name]
+        self.df[0]['sequence'] = pd.Series(range(len(self.df[0])))
+        y = self.df[0]['sequence']
+        self.x_train, self.x_test, self.y_train, self.y_test = \
+            train_test_split(x, y, test_size= 1 - train_percentage)
 
         return None
 
-
-    def linear_regression(self):
+    def simple_linear_regression(self):
+        self.x_train = self.x_train.values.reshape(-1, 1)
+        self.x_test = self.x_test.values.reshape(-1, 1)
+        self.y_train = self.y_train.values.reshape(-1, 1)
+        self.y_test = self.y_test.values.reshape(-1, 1)
         regr = linear_model.LinearRegression()
-        regr.fit(self.train_set_x, self.train_set_y)
-        self.prediction = regr.predict(self.test_set_x)
+        regr.fit(self.x_train, self.y_train)
+        self.prediction = regr.predict(self.x_test)
 
         print('Coefficients: \n', regr.coef_)
         # The mean squared error
         print('Mean squared error: %.2f'
-              % mean_squared_error(self.test_set_y, self.prediction))
+              % mean_squared_error(self.y_test.reshape(-1, 1), self.prediction))
         print('Coefficient of determination: %.2f'
-              % r2_score(self.test_set_y, self.prediction))
+              % r2_score(self.y_test.reshape(-1, 1), self.prediction))
 
-        plt.scatter(self.test_set_x, self.test_set_y, color='black')
-        plt.plot(self.test_set_x, self.prediction, color='blue', linewidth=3)
+        plt.scatter(self.x_test, self.y_test, color='black')
+        plt.plot(self.x_test, self.prediction, color='blue', linewidth=3)
         plt.xticks(())
         plt.yticks(())
         plt.show()
 
         return None
-
 
 
 input = LinearRegressionFit()
@@ -84,5 +78,5 @@ input.read_file(upload_folder, upload_files, sheet_name)
 path = os.getcwd()
 print(path)
 input.train_test_sets('FTSE_100', 0.8)
-input.linear_regression()
+input.simple_linear_regression()
 print('end')
